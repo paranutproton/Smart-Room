6a2eb833b98efceafed39c57d0c36bcf364ce477
Pattadon Naksuwan <pattag26@gmail.com>
2022-09-21 16:01:16 +0700

    fix the venv bug

--- sensor_data_serial.py
+++ sensor_data_serial.py
@@ -1,23 +1,23 @@
 import serial
 from datetime import datetime
-import pandas as pd
-import time
+# import pandas as pd
+# import time
 
-with serial.Serial('/dev/cu.usbmodem14101', 9600, timeout=1) as ser:
-    counter=0
+with serial.Serial('/dev/tty.usbmodem14103', 115200, timeout=1) as ser:
+    #counter=0
     while(True):
-        data = pd.read_csv('co2_data.csv')
+        #data = pd.read_csv('co2_data.csv')
         now = datetime.now()
         current_time = now.strftime("%d/%m/%Y %H:%M:%S")
         line = ser.readline()
         print(current_time, line.decode('UTF-8').strip().split())
-        counter = counter+1
-        if len(line.decode('UTF-8').strip().split())>3 and counter%30==0:
-            temp = pd.DataFrame()
-            temp['time'] = [current_time]
-            temp['CO2'] = [line.decode('UTF-8').strip().split()[1]]
-            temp['TVOC'] = [line.decode('UTF-8').strip().split()[4]]
-            data = pd.concat([data, temp])
-            data.to_csv('co2_data.csv', index=False)
-            print('saved!')
+        # counter = counter+1
+        # if len(line.decode('UTF-8').strip().split())>3 and counter%30==0:
+        #     temp = pd.DataFrame()
+        #     temp['time'] = [current_time]
+        #     temp['CO2'] = [line.decode('UTF-8').strip().split()[1]]
+        #     temp['TVOC'] = [line.decode('UTF-8').strip().split()[4]]
+        #     data = pd.concat([data, temp])
+        #     data.to_csv('co2_data.csv', index=False)
+        #     print('saved!')
     
