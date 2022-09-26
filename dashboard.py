7d13d61883e4fbf8c4c802841d352621c5479d20
Pattadon Naksuwan <pattag26@gmail.com>
2022-09-26 23:01:21 +0700

    enable the dashboard to control the servo to turn off the light

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,26 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
+import dash_daq as daq
+import serial
+import time
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1('Data Visualization of Sensors'),
-    dcc.Dropdown(['Temperature', 'Relative Humidity', 'Ultrasonic', 'Infrared'], 'Temperature', id='dropdown'),
+    dcc.Dropdown(['Temperature', 'Relative Humidity', 'Ultrasonic', 'Infrared', 'Light'], 'Temperature', id='dropdown'),
     dcc.Graph(id="graph"),
+    daq.BooleanSwitch(
+    on=False,
+    color="#9B51E0",
+    label="Turn this on to turn off the light",
+    id='my-boolean-switch'),
     dcc.Interval(
             id='interval-component',
             interval=1*1000, # in milliseconds
-            n_intervals=0)
+            n_intervals=0),
+    html.Div(id='boolean-switch-output')
 ])
 
 @app.callback(
@@ -32,5 +41,23 @@ def update_line_chart(sensor, n):
     elif sensor == 'Infrared':
         fig = px.line(data[['time','Detection']], x="time", y="Detection", title='Yes/No (1/0)')
         return fig
+    elif sensor == 'Light':
+        fig = px.line(data[['time','Light']], x="time", y="Light", title='Light Intensity')
+        return fig
+
+@app.callback(
+    Output('boolean-switch-output', 'children'),
+    Input('my-boolean-switch', 'on'))
+def move_servo(status):
+    serialcomm = serial.Serial('/dev/cu.usbmodem14403', 9600)
+    serialcomm.timeout = 1
+    if status == True:
+        serialcomm.write(b'1')
+        time.sleep(0.01)
+        return f'The light is off!'
+    else:
+        serialcomm.write(b'0')
+        time.sleep(0.01)
+        return f''
 
 app.run_server(debug=True)
