1cb9dc73879c29432775796b2757cf419cf2c022
paranutproton <paranutproton@gmail.com>
2022-11-04 11:14:44 +0700

    database implementation

--- sensor_data_serial.py
+++ sensor_data_serial.py
@@ -1,9 +1,16 @@
 import serial
 from datetime import datetime
 import pandas as pd
+import pandas_gbq
 import sys
+import time
 
-with serial.Serial('/dev/cu.usbmodem14403', 9600, timeout=1) as ser:
+def push_db():
+    data = pd.read_csv('sensors_data.csv')
+    data.to_gbq(destination_table='sensor_data.sensor_db',project_id='smartroom-db',if_exists='append')
+    print('pushed')
+
+with serial.Serial('/dev/cu.usbmodem1434203', 9600, timeout=1) as ser:
     counter=0
     if sys.argv[1] == 'co2':
         while(True):
@@ -56,18 +63,33 @@ with serial.Serial('/dev/cu.usbmodem14403', 9600, timeout=1) as ser:
             if line.decode('UTF-8').strip() != "":
                 data = pd.read_csv('sensors_data.csv')
                 now = datetime.now()
-                current_time = now.strftime("%d/%m/%Y %H:%M:%S")
-                print(current_time, line.decode('UTF-8').strip())
-                temp = pd.DataFrame()
-                temp['time'] = [current_time]
-                temp['Temperature'] = [float(line.decode('UTF-8').strip().split(", ")[0])]
-                temp['RH'] = [float(line.decode('UTF-8').strip().split(", ")[1])]
-                temp['Distance'] = [float(line.decode('UTF-8').strip().split(", ")[2])]
-                if line.decode('UTF-8').strip().split(", ")[3] == "Obstacle detected":
-                    temp['Detection'] = [1]
-                elif line.decode('UTF-8').strip().split(", ")[3] == "No obstacle detected":
-                    temp['Detection'] = [0]
-                temp['Light'] = [float(line.decode('UTF-8').strip().split(", ")[4])]
-                data = pd.concat([data, temp])
-                data.to_csv('sensors_data.csv', index=False)
-                print('saved!')
+                my_dates = pd.date_range("now", periods=1)
+                current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+                current_date = now.strftime("%Y-%m-%d")
+                counter = counter+1
+                if counter%30==0:
+                    temp = pd.DataFrame()
+                    temp['time'] = [current_time]
+                    temp['date'] =  [current_date]
+                    temp['Temperature'] = [float(line.decode('UTF-8').strip().split(", ")[0])]
+                    temp['RH'] = [float(line.decode('UTF-8').strip().split(", ")[1])]
+                    temp['Distance'] = [float(line.decode('UTF-8').strip().split(", ")[2])]
+                    if line.decode('UTF-8').strip().split(", ")[3] == "Obstacle detected":
+                        temp['Detection'] = [1]
+                    elif line.decode('UTF-8').strip().split(", ")[3] == "No obstacle detected":
+                        temp['Detection'] = [0]
+                    temp['Light'] = [float(line.decode('UTF-8').strip().split(", ")[4])]
+                    data = pd.concat([data, temp])
+                    data.to_csv('sensors_data.csv', index=False)
+                    print('saved!')
+                    print(type(now))
+                    if len(data) > 300:
+                        push_db()
+                        data_temp = pd.read_csv('data_temp.csv')
+                        data_temp.to_csv('sensors_data.csv', index=False)
+                        print('done push')
+                
+
+
+    
+    
