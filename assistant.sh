61d6417d4d78cf78de197e844367df78cd914b4b
gear-patt <pattag26@gmail.com>
2022-12-13 01:58:51 +0700

    enable automation mode

---
+++ assistant.sh
@@ -0,0 +1,3 @@
+. env/bin/activate
+cd /home/pro64/Desktop/Smart-Room/google-assistant/google-assistant-sdk/googlesamples/assistant/grpc
+python3 close_lights.py
--- sensor_data_serial.py
+++ sensor_data_serial.py
@@ -5,7 +5,10 @@ import pandas_gbq
 import sys
 import time
 from paho.mqtt import client as mqtt_client
-
+import pickle
+from sklearn.preprocessing import StandardScaler
+from joblib import load
+import subprocess
 
 broker = 'broker.hivemq.com'
 port = 1883
@@ -68,6 +71,9 @@ def push_db():
     data.to_gbq(destination_table='sensor_data.sensor_db',project_id='smartroom-db',if_exists='append')
     print('pushed')
 
+model = pickle.load(open('model.sav', 'rb'))
+sc = load('scaler.joblib')
+count_zero = 0
 
 with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
     counter=0
@@ -135,22 +141,45 @@ with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
                     current_date = now.strftime("%Y-%m-%d")
                     counter = counter+1
                     print(counter)
-                    if counter%60==0: #30 the best
+                    if counter%60==0:
+                        input_dt = []
+                        input_data = []
                         temp = pd.DataFrame()
                         temp['time'] = [current_time]
                         temp['date'] =  [current_date]
                         temp['Temperature'] = [float(line.decode('UTF-8').strip().split(", ")[0])]
+                        input_data.append(float(line.decode('UTF-8').strip().split(", ")[0]))
                         temp['RH'] = [float(line.decode('UTF-8').strip().split(", ")[1])]
+                        input_data.append(float(line.decode('UTF-8').strip().split(", ")[1]))
                         temp['Distance'] = [float(line.decode('UTF-8').strip().split(", ")[2])]
+                        input_data.append(float(line.decode('UTF-8').strip().split(", ")[2]))
                         if line.decode('UTF-8').strip().split(", ")[3] == "Obstacle detected":
                             temp['Detection'] = [1]
+                            input_data.append(1)
                         elif line.decode('UTF-8').strip().split(", ")[3] == "No obstacle detected":
                             temp['Detection'] = [0]
+                            input_data.append(0)
                         temp['Light'] = [float(line.decode('UTF-8').strip().split(", ")[4])]
+                        input_data.append(float(line.decode('UTF-8').strip().split(", ")[4]))
                         temp['Vibration'] = [int(vibration)]
+                        input_data.append(int(vibration))
                         temp['TVOC'] = [int(tvoc)]
+                        input_data.append(int(tvoc))
                         temp['CO2'] = [int(co2)]
+                        input_data.append(int(co2))
                         temp['PIR'] = [int(pir)]
+                        input_data.append(int(pir))
+                        input_dt.append(input_data)
+                        input_dt = sc.transform(input_dt)
+                        print(f'prediction: {model.predict(input_dt)}')
+                        if int(model.predict(input_dt)[0]) == 0:
+                            count_zero += 1
+                            if count_zero == 5:
+                                subprocess.call(["sh", "./assistant.sh"])
+                                print("The lights are closed!!")
+                                count_zero = 0
+                        else:
+                            count_zero = 0
                         data = pd.concat([data, temp])
                         real = pd.concat([real, temp])
                         data.to_csv('sensors_data.csv', index=False)
@@ -161,10 +190,6 @@ with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
                             data_temp = pd.read_csv('data_temp.csv')
                             data_temp.to_csv('sensors_data.csv', index=False)
                             print('done push')
-            except:
+            except Exception as e:
+                print(e)
                 continue
-
-
-
-    
-    
